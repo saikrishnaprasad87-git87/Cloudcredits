2773ff1e14465879403c772d807e376ba2a88d62
Sai Krishna Prasad <saikrishna.barkam87@gmail.com>
2025-04-06 10:39:53 +0530

    Re-added PasswordGenerator as a regular folder

---
+++ PasswordGenerator/src/PasswordGenerator/README.md
@@ -0,0 +1,2 @@
+# Cloudcredits
+This repository contains my Core Java Internship projects, including a Password Generator, Library Management systems.
---
+++ PasswordGenerator/src/com/project/passwordgenerator/PasswordGenerators.java
@@ -0,0 +1,10 @@
+package com.project.passwordgenerator;
+
+public class PasswordGenerators {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
